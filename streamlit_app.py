f37be1d0f6944a6df0dc8e0f62b58b4dbf590268
Marcelo Arias <hello@marceloarias.com>
2023-03-18 12:47:19 -0500

    Test Manim

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,12 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import manim
 from manim import *
 import os
 
 """
 # Welcome to Streamlit!
-
-Text Edition
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
@@ -19,54 +18,24 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-class Example(Scene):
-    def construct(self):
-        circle = Circle(color=WHITE)
-        square = Square(color=BLUE)
 
-        self.play(Create(circle))
-        self.wait()
+with st.echo(code_location='below'):
+    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-        self.play(Transform(circle, square))
-        self.wait()
+    Point = namedtuple('Point', 'x y')
+    data = []
 
-        self.play(FadeOut(square))
-        self.wait()
+    points_per_turn = total_points / num_turns
 
-# Define the Streamlit app
-def main():
-    st.title("Basic Manim Animation")
+    for curr_point_num in range(total_points):
+        curr_turn, i = divmod(curr_point_num, points_per_turn)
+        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+        radius = curr_point_num / total_points
+        x = radius * math.cos(angle)
+        y = radius * math.sin(angle)
+        data.append(Point(x, y))
 
-    # Create a button to run the Manim animation
-    if st.button("Run Animation"):
-        # Render the animation using Manim
-        scene = Example()
-        scene.render()
-
-        # Display the rendered video using Streamlit
-        video_path = os.path.join(os.getcwd(), "media/videos/Example.mp4")
-        st.video(video_path)
-
-if __name__ == "__main__":
-    main()
-
-# with st.echo(code_location='below'):
-    # total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    # num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    # Point = namedtuple('Point', 'x y')
-    # data = []
-
-    # points_per_turn = total_points / num_turns
-
-    # for curr_point_num in range(total_points):
-        # curr_turn, i = divmod(curr_point_num, points_per_turn)
-        # angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        # radius = curr_point_num / total_points
-        # x = radius * math.cos(angle)
-        # y = radius * math.sin(angle)
-        # data.append(Point(x, y))
-
-    # st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        # .mark_circle(color='#0068c9', opacity=0.5)
-        # .encode(x='x:Q', y='y:Q'))
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
