d78d209d69e364424d1b3fd5e73d8ca894b93006
Marcelo Arias <hello@marceloarias.com>
2023-03-18 16:39:09 -0500

    Test Manim

--- scene.py
+++ scene.py
@@ -1,18 +1,18 @@
-from manim import *
+# from manim import *
 
-class Example(Scene):
-    def construct(self):
-        circle = Circle(color=WHITE)
-        square = Square(color=BLUE)
+# class Example(Scene):
+#     def construct(self):
+#         circle = Circle(color=WHITE)
+#         square = Square(color=BLUE)
 
-        self.play(Create(circle))
-        self.wait()
+#         self.play(Create(circle))
+#         self.wait()
 
-        self.play(Transform(circle, square))
-        self.wait()
+#         self.play(Transform(circle, square))
+#         self.wait()
 
-        self.play(FadeOut(square))
-        self.wait()
+#         self.play(FadeOut(square))
+#         self.wait()
 
-scene = Example()
-scene.render(preview=False)
+# scene = Example()
+# scene.render(preview=False)
