441059852283486de941c138dcb6596e519320c7
Marcelo Arias <hello@marceloarias.com>
2023-03-18 15:03:46 -0500

    Test Manim

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,20 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+class Example(Scene):
+    def construct(self):
+        circle = Circle(color=WHITE)
+        square = Square(color=BLUE)
+
+        self.play(Create(circle))
+        self.wait()
+
+        self.play(Transform(circle, square))
+        self.wait()
+
+        self.play(FadeOut(square))
+        self.wait()
+
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
