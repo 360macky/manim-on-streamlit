af52c1996c964b79a9912da09ec3beca821ff833
Marcelo Arias <hello@marceloarias.com>
2023-03-18 15:30:03 -0500

    Test Manim

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,9 @@ def main():
         scene = Example()
         scene.render()
 
+        image_path = "/app/streamlit-example/media/images/Example_ManimCE_v0.16.0.post0.png"
+        st.image(image_path, caption="Example Image")
+
         # Display the rendered video using Streamlit
         # video_path = os.path.join(os.getcwd(), "media/videos/Example.mp4")
         # st.video(video_path)
